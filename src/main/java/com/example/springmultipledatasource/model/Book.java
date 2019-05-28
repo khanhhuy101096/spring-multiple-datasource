d99b1dbec9ed9baab6049eb3f0d16fdbf3d9aaec
khanhhuy101096 <khanhhuy101096@gmail.com>
2019-05-28 09:34:29 +0700

    Auto getter, setter, create contructor with loombok lib

--- src/main/java/com/example/springmultipledatasource/model/Book.java
+++ src/main/java/com/example/springmultipledatasource/model/Book.java
@@ -8,38 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-//@Table(name="BOOK_TB")
+@Table(name="BOOK_TB")
 public class Book {
 
     @Id
     private int id;
     private String name;
-
-    public Book() {
-    }
-
-    public Book(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/com/example/springmultipledatasource/model/User.java
+++ src/main/java/com/example/springmultipledatasource/model/User.java
@@ -6,9 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name="USER_TB")
 public class User {
@@ -16,28 +16,4 @@ public class User {
     @Id
     private int id;
     private String userName;
-
-    public User() {
-    }
-
-    public User(int id, String userName) {
-        this.id = id;
-        this.userName = userName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
 }
